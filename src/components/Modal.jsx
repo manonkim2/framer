52a649fcf73d54a85e0575ecdd0e9492f3dab0d2
Manon Kim <kim.manon2@gmail.com>
2024-10-10 17:33:30 +0900

    [feat] initial

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,20 @@
 import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 2 }}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById("modal")
   );
